ce891cd5eb7b96898730b5eee9a69780b62ba90d
Arvin <gnimoaix857@gmail.com>
2022-04-22 11:42:01 +1000

    Added animation to virtual keyboard

--- lib/app/widgets/virtual_keyboard.dart
+++ lib/app/widgets/virtual_keyboard.dart
@@ -49,154 +49,125 @@ class _VirtualKeyboardState extends State<VirtualKeyboard> {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         for (var i = 0; i < 4; i++)
-          Builder(builder: (context) {
-            if (i < 3) {
-              k += (i == 0) ? 0 : (i - 1);
-              return Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  for (var j = i; j < 10; j++)
-                    Padding(
-                      padding: EdgeInsets.all(spacing),
-                      child: SizedBox(
-                        height: size,
-                        width: size,
-                        child: Builder(
-                          builder: (context) {
-                            final index = (i * 9) + j - k;
-                            if (index < 26) {
-                              final virtualKey = controller.keyAt(index);
-                              var backgroundColor = Colors.transparent;
-                              switch (controller.statusAt(virtualKey)) {
-                                case LetterStatus.wrongPosition:
-                                  backgroundColor = Colors.amber;
-                                  break;
-                                case LetterStatus.wrong:
-                                  backgroundColor = Colors.grey.shade400;
-                                  break;
-                                case LetterStatus.correct:
-                                  backgroundColor = Colors.lime;
-                                  break;
-                                case LetterStatus.empty:
-                                  break;
-                              }
+          Theme(
+            data: Theme.of(context).copyWith(
+              outlinedButtonTheme: OutlinedButtonThemeData(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.black),
+                  overlayColor: MaterialStateProperty.resolveWith((states) {
+                    if (states.isNotEmpty) {
+                      switch (states.first) {
+                        case MaterialState.pressed:
+                          return Colors.blue.withOpacity(0.3);
+                        default:
+                          break;
+                      }
+                    }
+                    return Colors.transparent;
+                  }),
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.transparent),
+                  side: MaterialStateProperty.resolveWith((states) {
+                    late var color = Colors.grey.shade300;
+                    if (states.isNotEmpty) {
+                      final state = states.first;
+                      if (state == MaterialState.hovered ||
+                          state == MaterialState.pressed) {
+                        color = Colors.black;
+                      }
+                    }
+                    return BorderSide(color: color, width: 1);
+                  }),
+                ),
+              ),
+            ),
+            child: Builder(builder: (context) {
+              if (i < 3) {
+                k += (i == 0) ? 0 : (i - 1);
+                return Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    for (var j = i; j < 10; j++)
+                      Padding(
+                        padding: EdgeInsets.all(spacing),
+                        child: SizedBox(
+                          height: size,
+                          width: size,
+                          child: Builder(
+                            builder: (context) {
+                              final index = (i * 9) + j - k;
+                              if (index < 26) {
+                                final virtualKey = controller.keyAt(index);
+                                var backgroundColor = Colors.transparent;
+                                switch (controller.statusAt(virtualKey)) {
+                                  case LetterStatus.wrongPosition:
+                                    backgroundColor = Colors.amber;
+                                    break;
+                                  case LetterStatus.wrong:
+                                    backgroundColor = Colors.grey.shade400;
+                                    break;
+                                  case LetterStatus.correct:
+                                    backgroundColor = Colors.lime;
+                                    break;
+                                  case LetterStatus.empty:
+                                    break;
+                                }
 
+                                return AnimatedContainer(
+                                  color: backgroundColor,
+                                  duration: const Duration(milliseconds: 500),
+                                  curve: Curves.easeInToLinear,
+                                  child: OutlinedButton(
+                                    onPressed: (onLetterKeyPressed == null)
+                                        ? null
+                                        : () {
+                                            onLetterKeyPressed(virtualKey);
+                                            onKeyPressed?.call();
+                                          },
+                                    child: Text(
+                                      virtualKey,
+                                    ),
+                                  ),
+                                );
+                              }
                               return OutlinedButton(
-                                onPressed: (onLetterKeyPressed == null)
+                                onPressed: (onBackspacePressed == null)
                                     ? null
                                     : () {
-                                        onLetterKeyPressed(virtualKey);
+                                        onBackspacePressed();
                                         onKeyPressed?.call();
                                       },
-                                child: Text(
-                                  virtualKey,
-                                ),
-                                style: ButtonStyle(
-                                  backgroundColor: MaterialStateProperty.all(
-                                      backgroundColor),
-                                  foregroundColor:
-                                      MaterialStateProperty.all(Colors.black),
-                                  overlayColor:
-                                      MaterialStateProperty.resolveWith(
-                                          (states) {
-                                    if (states.isNotEmpty) {
-                                      switch (states.first) {
-                                        case MaterialState.hovered:
-                                          if (backgroundColor ==
-                                              Colors.transparent) {
-                                            return Colors.blue.withOpacity(0.1);
-                                          }
-                                          return Colors.white.withOpacity(0.1);
-                                        case MaterialState.pressed:
-                                          if (backgroundColor ==
-                                              Colors.transparent) {
-                                            return Colors.blue.withOpacity(0.1);
-                                          }
-                                          return Colors.white.withOpacity(0.1);
-                                        default:
-                                          break;
-                                      }
-                                    }
-                                    return Colors.transparent;
-                                  }),
+                                child: const Icon(
+                                  Icons.backspace_outlined,
+                                  size: 18,
                                 ),
                               );
-                            }
-                            return OutlinedButton(
-                              onPressed: (onBackspacePressed == null)
-                                  ? null
-                                  : () {
-                                      onBackspacePressed();
-                                      onKeyPressed?.call();
-                                    },
-                              child: const Icon(
-                                Icons.backspace_outlined,
-                                size: 18,
-                              ),
-                              style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.transparent),
-                                foregroundColor:
-                                    MaterialStateProperty.all(Colors.black),
-                                overlayColor:
-                                    MaterialStateProperty.resolveWith((states) {
-                                  if (states.isNotEmpty) {
-                                    switch (states.first) {
-                                      case MaterialState.hovered:
-                                        return Colors.blue.withOpacity(0.1);
-                                      case MaterialState.pressed:
-                                        return Colors.blue.withOpacity(0.1);
-                                      default:
-                                        break;
-                                    }
-                                  }
-                                  return Colors.transparent;
-                                }),
-                              ),
-                            );
-                          },
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                ],
-              );
-            } else {
-              return Padding(
-                padding: EdgeInsets.all(spacing),
-                child: SizedBox(
-                  height: size,
-                  width: (size * 7) + (spacing * 6 * 2),
-                  child: OutlinedButton(
-                    child: const Text('Enter'),
-                    onPressed: (onEnterPressed == null)
-                        ? null
-                        : () {
-                            onEnterPressed();
-                            onKeyPressed?.call();
-                          },
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(Colors.transparent),
-                      foregroundColor: MaterialStateProperty.all(Colors.black),
-                      overlayColor: MaterialStateProperty.resolveWith((states) {
-                        if (states.isNotEmpty) {
-                          switch (states.first) {
-                            case MaterialState.hovered:
-                              return Colors.blue.withOpacity(0.1);
-                            case MaterialState.pressed:
-                              return Colors.blue.withOpacity(0.1);
-                            default:
-                              break;
-                          }
-                        }
-                        return Colors.transparent;
-                      }),
+                  ],
+                );
+              } else {
+                return Padding(
+                  padding: EdgeInsets.all(spacing),
+                  child: SizedBox(
+                    height: size,
+                    width: (size * 7) + (spacing * 6 * 2),
+                    child: OutlinedButton(
+                      child: const Text('Enter'),
+                      onPressed: (onEnterPressed == null)
+                          ? null
+                          : () {
+                              onEnterPressed();
+                              onKeyPressed?.call();
+                            },
                     ),
                   ),
-                ),
-              );
-            }
-          }),
+                );
+              }
+            }),
+          ),
       ],
     );
   }
